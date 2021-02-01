0dc7880842f49be3ce7a0bbf8b8fcae82d31fc76
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-02-01 17:26:27 +0100

    add shiftKey method for scrambling bytes, making the cipher more difficult to crack

--- src/com/company/JMDSuperEncryption.java
+++ src/com/company/JMDSuperEncryption.java
@@ -12,6 +12,8 @@ class JMDSuperEncryption {
 
         byte[] keyBytes = key.toByteArray();
 
+        shiftKey(keyBytes);
+
         if (keyBytes.length > text.length()) {
             Character[] distinctChars = distinctChars(text.toCharArray());
             // Add salt
@@ -35,6 +37,8 @@ class JMDSuperEncryption {
 
         byte[] keyBytes = key.toByteArray();
 
+        shiftKey(keyBytes);
+
         for (int i = 0; i < cipher.length(); i++) {
             char ch = (char) (((int) cipher.charAt(i) - keyBytes[i % (keyBytes.length - 1)]));
             result.append(ch);
@@ -42,6 +46,20 @@ class JMDSuperEncryption {
 
         // Remove salt
         return new StringBuffer(result.substring(0, keyBytes[0]));
+    }
+
+    /**
+     * Shifts each byte in the key by the length of the string.
+     * Alternates alternates between (+) and (-).
+     * @param key byte representation of the key
+     */
+    private static void shiftKey(byte[] key) {
+        for (int i = 1; i < key.length; i++) {
+            if (i % 2 == 0)
+                key[i] += key[0];
+            else
+                key[i] -= key[0];
+        }
     }
 
     private static Character[] distinctChars(char[] array) {
@@ -58,25 +76,31 @@ class JMDSuperEncryption {
         byte[] bytes = new byte[keyLength];
         bytes[0] = (byte) plainText.length();
         for (int i = 1; i < keyLength; i++) {
-            bytes[i] = (byte) (random.nextInt(255) - 128);
+            bytes[i] = (byte) (random.nextInt(255) - 128); // -128 to 127
         }
 
         return new BigInteger(bytes);
     }
 
     public static void main(String[] args) {
-        String originalText = "Password123";
+        String originalText = "Testing JMDSuperEncryption!";
 
-        BigInteger key = generateKey(100, originalText);
+        BigInteger key = generateKey(50, originalText);
 
         System.out.println("Key: " + key);
         System.out.println("Key Bytes: " + Arrays.toString(key.toByteArray()));
         System.out.println();
         System.out.println("Encryption");
         System.out.println("Text: " + originalText);
+
         String cipher = encrypt(originalText, key).toString();
         System.out.println("Encrypted Cipher: " + cipher);
+
+        /*byte[] keyBytes = key.toByteArray();
+        keyBytes[0] = 27;
+        key = new BigInteger(keyBytes);*/
+
         String decryptedPlainText = decrypt(cipher, key).toString();
-        System.out.println("Decrypted Plain Text : " + decryptedPlainText);
+        System.out.println("Decrypted Plain Text: " + decryptedPlainText);
     }
 }
