bb352c3f7afc77ae81f613464f6b39d6e2ef48b3
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-02-01 15:40:30 +0100

    add salt before encryption to hide string length. first index of key contains string length for decrypting.

--- src/com/company/JMDSuperEncryption.java
+++ src/com/company/JMDSuperEncryption.java
@@ -1,52 +1,82 @@
 package com.company;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.HashSet;
 
 class JMDSuperEncryption {
 
     public static StringBuffer encrypt(String text, BigInteger key) {
-        StringBuffer result = new StringBuffer();
+        var result = new StringBuffer();
 
         byte[] keyBytes = key.toByteArray();
+
+        if (keyBytes.length > text.length()) {
+            Character[] distinctChars = distinctChars(text.toCharArray());
+            // Add salt
+            for (int i = 0; i < keyBytes.length - text.length(); i++) {
+                var random = new SecureRandom();
+                int randomIndex = random.nextInt(distinctChars.length);
+                text += distinctChars[randomIndex];
+            }
+        }
 
         for (int i = 0; i < text.length(); i++) {
             char ch = (char) (((int) text.charAt(i) + keyBytes[i % (keyBytes.length - 1)]));
             result.append(ch);
         }
+
         return result;
     }
 
     public static StringBuffer decrypt(String cipher, BigInteger key) {
-        StringBuffer result = new StringBuffer();
+        var result = new StringBuilder();
 
         byte[] keyBytes = key.toByteArray();
 
-        for (int i = 0; i < keyBytes.length; i++) {
-            keyBytes[i] = (byte)(256 - keyBytes[i]);
-        }
-
         for (int i = 0; i < cipher.length(); i++) {
-            char ch = (char) (((int) cipher.charAt(i) + keyBytes[i % (keyBytes.length - 1)]));
+            char ch = (char) (((int) cipher.charAt(i) - keyBytes[i % (keyBytes.length - 1)]));
             result.append(ch);
         }
-        return result;
+
+        // Remove salt
+        return new StringBuffer(result.substring(0, keyBytes[0]));
+    }
+
+    private static Character[] distinctChars(char[] array) {
+        var set = new HashSet<Character>();
+        for (char c : array) {
+            set.add(c);
+        }
+        return set.toArray(Character[]::new);
+    }
+
+    private static BigInteger generateKey(int keyLength, String plainText) {
+        var random = new SecureRandom();
+
+        byte[] bytes = new byte[keyLength];
+        bytes[0] = (byte) plainText.length();
+        for (int i = 1; i < keyLength; i++) {
+            bytes[i] = (byte) (random.nextInt(255) - 128);
+        }
+
+        return new BigInteger(bytes);
     }
 
     public static void main(String[] args) {
-        String originalText = "Efter att tidningen Göteborgs-Posten avslöjat att Ola Serneke skrivit inlägg under pseudonym, som kan ha gynnat hans eget bolag på Placeras börsforum, kräver nu organisationen Aktiespararna hans omedelbara avgång.";
-        byte[] shiftCount = { 24, 12, 18, 6, 89, 24, 11, 1, -127 };
+        String originalText = "Password123";
 
-        BigInteger bigInt = new BigInteger(shiftCount);
-        System.out.println("Key: " + bigInt);
+        BigInteger key = generateKey(100, originalText);
 
-        System.out.println("Example");
+        System.out.println("Key: " + key);
+        System.out.println("Key Bytes: " + Arrays.toString(key.toByteArray()));
+        System.out.println();
         System.out.println("Encryption");
-        System.out.println("Text  : " + originalText);
-        System.out.println("Key Bytes : " + Arrays.toString(bigInt.toByteArray()));
-        String cipher = encrypt(originalText, bigInt).toString();
+        System.out.println("Text: " + originalText);
+        String cipher = encrypt(originalText, key).toString();
         System.out.println("Encrypted Cipher: " + cipher);
-        String decryptedPlainText = decrypt(cipher, bigInt).toString();
-        System.out.println("Decrypted Plain Text  : " + decryptedPlainText);
+        String decryptedPlainText = decrypt(cipher, key).toString();
+        System.out.println("Decrypted Plain Text : " + decryptedPlainText);
     }
 }
